5f0bdd2696cf0a6d778b68079365e297de4d4b4d
fisteatcheese <whatzit293@outlook.com>
2026-03-20 02:19:23 -0400

    added cursor and styling

---
+++ iterate.module.static.js
@@ -0,0 +1,30 @@
+function iterate(items, items2 = [], items3 = []) {
+    items.forEach(item => {
+        item.addEventListener('mouseenter', function() {
+            fixCusor(false);
+        });
+        item.addEventListener('mouseleave', function() {
+            fixCusor(true);
+        });
+    });
+    if (items2.length > 0) {
+        items2.forEach(item => {
+            item.addEventListener('mouseenter', function() {
+                fixCusor(false);
+            });
+            item.addEventListener('mouseleave', function() {
+                fixCusor(true);
+            });
+        }); 
+    }
+    if (items3.length > 0) {
+        items3.forEach(item => {
+            item.addEventListener('mouseenter', function() {
+                fixCusor(false);
+            });
+            item.addEventListener('mouseleave', function() {
+                fixCusor(true);
+            });
+        }); 
+    }
+}
